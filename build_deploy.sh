3a3bee31aeccfb9b32746fca1912db0ef5a7a216
Joshua Sindy <josh@root.bz>
2022-02-22 09:11:22 -0500

    update permissions

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t hbarwatch:latest .
+docker build . --file Dockerfile --tag hbarwatch:dev-$(date +%s) --tag hbarwatch:dev-latest
 docker rm -f hbarwatch
-docker run -d -p 80:80 --restart unless-stopped --name hbarwatch hbarwatch:latest
+docker run -d -p 80:80 --restart unless-stopped --name hbarwatch hbarwatch:dev-latest
